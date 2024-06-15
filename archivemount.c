c6c16fbcce4d541fc27aed1a08a100c2b84e0308
наб <nabijaczleweli@nabijaczleweli.xyz>
2024-06-15 19:37:38 +0200

    debug builds write to stderr instead of /tmp/archivemount.log

--- archivemount.c
+++ archivemount.c
@@ -59,14 +59,7 @@
 #ifdef NDEBUG
 #define log(format, ...)
 #else
-#define log(format, ...)                                            \
-	{                                                                 \
-		FILE * FH = fopen("/tmp/archivemount.log", "a");                \
-		if(FH) {                                                        \
-			fprintf(FH, "l. %4d: " format "\n", __LINE__, ##__VA_ARGS__); \
-			fclose(FH);                                                   \
-		}                                                               \
-	}
+#define log(format, ...) fprintf(stderr, "l. %4d: " format "\n", __LINE__, ##__VA_ARGS__)
 #endif
 
 
